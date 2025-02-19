e57e404753005dcc6a81cf63f3eb79a873fcaf94
dhruv sharma <dhruvsharma.main@gmail.com>
2025-02-19 16:04:30 +0530

    Added Task-02 Blog Website

---
+++ Task-02_BlogWebsite/src/components/Home/Write/Preview.jsx
@@ -0,0 +1,174 @@
+import { addDoc, collection } from "firebase/firestore";
+import React, { useEffect, useRef, useState } from "react";
+import { LiaTimesSolid } from "react-icons/lia";
+import ReactQuill from "react-quill";
+import TagsInput from "react-tagsinput";
+import { toast } from "react-toastify";
+import { db } from "../../../firebase/firebase";
+import { Blog } from "../../../Context/Context";
+import { useNavigate } from "react-router-dom";
+
+const Preview = ({ setPublish, description, title }) => {
+  const imageRef = useRef(null);
+  const [imageUrl, setImageUrl] = useState("");
+  const [tags, setTags] = useState([]);
+  const [desc, setDesc] = useState("");
+  const { currentUser } = Blog();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const [preview, setPreview] = useState({
+    title: "",
+    photo: "",
+  });
+
+  useEffect(() => {
+    if (title || description) {
+      setPreview((prev) => ({ ...prev, title }));
+      setDesc(description);
+    } else {
+      setPreview({ title: "", photo: "" });
+      setDesc("");
+    }
+  }, [title, description]);
+
+  const handleClick = () => {
+    imageRef.current.click();
+  };
+
+  // Upload Image to Cloudinary
+  const uploadImageToCloudinary = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+    formData.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
+  
+    try {
+      const response = await fetch(
+        `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      const data = await response.json();
+      return data.secure_url;
+    } catch (error) {
+      console.error("Cloudinary Upload Error:", error);
+      return null;
+    }
+  };
+  
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    try {
+      if (!preview.title || !desc || tags.length === 0) {
+        toast.error("All fields are required!!!");
+        return;
+      }
+
+      if (preview.title.length < 5) {
+        toast.error("Title must be at least 5 letters");
+        return;
+      }
+
+      let imageUrlToSave = "";
+      if (preview.photo) {
+        imageUrlToSave = await uploadImageToCloudinary(preview.photo);
+      }
+
+      await addDoc(collection(db, "posts"), {
+        userId: currentUser?.uid,
+        title: preview.title,
+        desc,
+        tags,
+        postImg: imageUrlToSave || "",
+        created: Date.now(),
+        pageViews: 0,
+      });
+      toast.success("Post has been added");
+      navigate("/");
+      setPublish(false);
+      setPreview({ title: "", photo: "" });
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <section className="absolute inset-0 bg-white z-30">
+      <div className="size my-[2rem]">
+        <span
+          onClick={() => setPublish(false)}
+          className="absolute right-[1rem] md:right-[5rem] top-[3rem] text-2xl cursor-pointer">
+          <LiaTimesSolid />
+        </span>
+        <div className="mt-[8rem] flex flex-col md:flex-row gap-10">
+          <div className="flex-[1]">
+            <h3>Story Preview</h3>
+            <div
+              style={{ backgroundImage: `url(${imageUrl})` }}
+              onClick={handleClick}
+              className="w-full h-[200px] object-cover bg-gray-100 my-3 grid 
+                place-items-center cursor-pointer bg-cover bg-no-repeat">
+              {!imageUrl && "Add Image"}
+            </div>
+            <input
+              onChange={(e) => {
+                const file = e.target.files[0];
+                if (file) {
+                  setImageUrl(URL.createObjectURL(file));
+                  setPreview((prev) => ({ ...prev, photo: file }));
+                }
+              }}
+              ref={imageRef}
+              type="file"
+              hidden
+            />
+            <input
+              type="text"
+              placeholder="Title"
+              className="outline-none w-full border-b border-gray-300 py-2"
+              value={preview.title}
+              onChange={(e) =>
+                setPreview((prev) => ({ ...prev, title: e.target.value }))
+              }
+            />
+            <ReactQuill
+              theme="bubble"
+              value={desc}
+              onChange={setDesc}
+              placeholder="Tell Your Story..."
+              className="py-3 border-b border-gray-300"
+            />
+            <p className="text-gray-500 pt-4 text-sm">
+              <span className="font-bold">Note:</span> Changes here will affect
+              how your story appears in public places like Medium’s homepage and
+              in subscribers’ inboxes — not the contents of the story itself.
+            </p>
+          </div>
+          <div className="flex-[1] flex flex-col gap-4 mb-5 md:mb-0">
+            <h3 className="text-2xl">
+              Publishing to: <span className="font-bold capitalize">Milad Tech</span>
+            </h3>
+            <p>
+              Add or change topics up to 5 so readers know what your story is
+              about
+            </p>
+            <TagsInput value={tags} onChange={setTags} />
+            <button
+              onClick={handleSubmit}
+              className="btn !bg-green-800 !w-fit !text-white !rounded-full">
+              {loading ? "Submitting..." : "Publish Now"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Preview;
---
+++ Task-02_BlogWebsite/src/utils/Modal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Modal = ({ children, modal, setModal }) => {
+  return (
+    <>
+      <div
+        onClick={() => setModal(false)}
+        className={`bg-white/95 fixed inset-0 z-10 
+      ${
+        modal ? "visible opacity-100" : "invisible opacity-0"
+      } transition-all duration-300`}
+      />
+      {children}
+    </>
+  );
+};
+
+export default Modal;
