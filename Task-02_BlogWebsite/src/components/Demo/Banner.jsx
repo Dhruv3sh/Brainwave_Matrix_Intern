a3c0ce245058d8badf72472fa80f4db3b57fca1e
dhruv sharma <dhruvsharma.main@gmail.com>
2025-02-20 22:00:25 +0530

    update: added banner image

--- Task-02_BlogWebsite/src/components/Demo/Banner.jsx
+++ Task-02_BlogWebsite/src/components/Demo/Banner.jsx
@@ -8,14 +8,13 @@ const Banner = () => {
     const { authModel, setAuthModel } = Blog();
 
   return (
-    <div className="bg-[#F7F4ED] border-b border-black">
+    <div className="bg-[#F7F4ED] border-b border-black relative ">
       <div className="size py-[5rem] flex flex-col items-start gap-[1rem]">
-        
         <TypeAnimation
               sequence={[
-                "Stay Curious..",
+                "Stay Curious.",
                 2000,
-                "Dive Deep Into Blog.",
+                "Read Wider.",
                 1000,
               ]}
               speed={50}
@@ -29,6 +28,9 @@ const Banner = () => {
         <button className="btn bg-black1 rounded-full text-white !text-[1.2rem] !px-6 !mt-[2.5rem]" onClick={() => setAuthModel(true)}>
           Start reading
         </button>
+      </div>
+      <div className="absolute top-0 right-[4.5rem] hidden lg:block" >
+        <img src="../demoBannerImg.webp" alt="img" width='360' />
       </div>
     </div>
   );
