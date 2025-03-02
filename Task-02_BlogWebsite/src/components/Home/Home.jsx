eef25043b7c62669315dec1a8f54a88bfa5c850e
dhruv sharma <dhruvsharma.main@gmail.com>
2025-03-03 00:56:45 +0530

    fix: styling

--- Task-02_BlogWebsite/src/components/Home/Home.jsx
+++ Task-02_BlogWebsite/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Posts from "../Common/Posts/Posts";
 import Follow from "./UserToFollow/Follow";
+import Discover from "../Demo/Discover";
 
 const Home = () => {
   return (
@@ -8,9 +9,12 @@ const Home = () => {
       <div className="flex-[2] py-10 mb-[4rem]">
         <Posts />
       </div>
-      <div className="hidden md:inline-block md:w-[21rem] p-7 border-l border-gray-300">
+      <div className="hidden lg:inline-block md:w-[21rem] p-7 border-l border-gray-300">
         <h3>Who to follow?</h3>
         <Follow />
+        <div className=" sticky top-[5rem] mt-8">
+        <Discover/>
+        </div>
       </div>
     </section>
   );
